6e252d4dc0899c9a1174432facd1b456ae411558
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2022-04-25 16:28:21 +0500

    update config.js

--- config.js
+++ config.js
@@ -3,3 +3,8 @@ dotenv.config()
 
 
 process.env.JWT_EXP = 60 * 60 * 60 * 24
+
+process.DEFAULTS = {}
+process.DEFAULTS.pagination = {}
+process.DEFAULTS.pagination.page = 1
+process.DEFAULTS.pagination.limit = 10
